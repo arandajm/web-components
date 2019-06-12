310b92b3b8eaeb611790047cfb7acd3054c9aa35
Jesus Aranda <aranda.jm@icloud.com>
2019-06-12 15:54:24 +0100

    Using attributes on custom elements

--- tooltip.js
+++ tooltip.js
@@ -3,10 +3,15 @@ class Tooltip extends HTMLElement {
     super();
     // This will be used to save the tooltip container and it will be accessed into the class
     this.tooltipContainer;
+    // This property is associated with the text attribute, that for default has "Some dummy text!!" like its value
+    this.tooltipText = "Some dummy text!!";
   }
 
   //Access to the DOM when this element has been mounted and modify it!
   connectedCallback() {
+    if (this.hasAttribute("text")) {
+      this.tooltipText = this.getAttribute("text");
+    }
     const tooltipIcon = document.createElement("span");
     tooltipIcon.textContent = " (?)";
     //Add listener when mouse is enter
@@ -18,7 +23,7 @@ class Tooltip extends HTMLElement {
   // This method should be used into the class ("_ convension")
   _showTooltip() {
     this.tooltipContainer = document.createElement("div");
-    this.tooltipContainer.textContent = "This is the tooltip text!!!";
+    this.tooltipContainer.textContent = this.tooltipText;
     this.appendChild(this.tooltipContainer);
   }
 
