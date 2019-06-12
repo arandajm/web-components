e30597242d71a7883351d2843876a7677b12f656
Jesus Aranda <aranda.jm@icloud.com>
2019-06-12 18:39:07 +0100

    enable sahodw DOM in web component

--- tooltip.js
+++ tooltip.js
@@ -5,6 +5,8 @@ class Tooltip extends HTMLElement {
     this.tooltipContainer;
     // This property is associated with the text attribute, that for default has "Some dummy text!!" like its value
     this.tooltipText = "Some dummy text!!";
+    //Enable shadow DOM
+    this.attachShadow({ mode: "open" });
   }
 
   //Access to the DOM when this element has been mounted and modify it!
@@ -17,18 +19,24 @@ class Tooltip extends HTMLElement {
     //Add listener when mouse is enter
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = "relative";
   }
 
   // This method should be used into the class ("_ convension")
   _showTooltip() {
     this.tooltipContainer = document.createElement("div");
     this.tooltipContainer.textContent = this.tooltipText;
-    this.appendChild(this.tooltipContainer);
+    this.tooltipContainer.style.backgroundColor = "black";
+    this.tooltipContainer.style.color = "white";
+    this.tooltipContainer.style.position = "absolute";
+    this.tooltipContainer.style.zIndex = "10";
+
+    this.shadowRoot.appendChild(this.tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this.tooltipContainer);
+    this.shadowRoot.removeChild(this.tooltipContainer);
   }
 }
 
