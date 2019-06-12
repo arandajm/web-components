f3bdaa6f210851bfc916a23505f8174e5b6b4373
Jesus Aranda <aranda.jm@icloud.com>
2019-06-12 15:21:28 +0100

    Using connectedcallback for DOM

--- tooltip.js
+++ tooltip.js
@@ -1,7 +1,13 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
-    console.log("It is working!!");
+  }
+
+  //Access to the DOM when this element has been mounted and modify it!
+  connectedCallback() {
+    const tooltipIcon = document.createElement("span");
+    tooltipIcon.textContent = " (?)";
+    this.appendChild(tooltipIcon);
   }
 }
 
